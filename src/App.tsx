d741f304428dd09781131f65acdbd776839fd93e
arielrahmane <arielrahmane@gmail.com>
2022-04-24 14:23:59 +1000

    navbar better styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { N1, N1Item, N1Props } from './components/Navbars/N1';
-
-const navbarItems: N1Item[] = [
-  {
-    name: 'Pictures',
-    href: 'pictures',
-  },
-  {
-    name: 'Item2',
-  },
-];
+import { N1, N1Props } from './components/Navbars/N1';
 
 const navbarProps: N1Props = {
-  items: navbarItems,
-  heading: 'Navbar Test',
+  heading: 'Navbar Component',
+  items: [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'About Us',
+      href: 'about',
+    },
+    {
+      label: 'Docs',
+      href: 'docs',
+    },
+    {
+      label: 'FAQs',
+      href: 'faqs',
+    },
+    {
+      label: 'Contact',
+      href: 'contact',
+    },
+  ],
+  specialItems: [
+    {
+      label: 'Login',
+      href: 'login',
+    },
+    {
+      label: 'Register',
+      href: 'register',
+    },
+  ],
   backgroundColor: 'bg-amber-500',
 };
 
@@ -22,6 +43,7 @@ function App() {
     <div>
       <N1
         items={navbarProps.items}
+        specialItems={navbarProps.specialItems}
         heading={navbarProps.heading}
         backgroundColor={navbarProps.backgroundColor}
       />
--- src/components/Navbars/N1.tsx
+++ src/components/Navbars/N1.tsx
@@ -2,14 +2,16 @@ import classNames from 'classnames';
 import React from 'react';
 
 export interface N1Item {
-  name: string;
-  href?: string;
+  label: string;
+  href: string;
 }
 
 export interface N1Props {
   /** Mandatory props */
   items: N1Item[];
   heading: string;
+  /** Optional Props */
+  specialItems?: N1Item[];
   /** Styling props */
   backgroundColor?: string;
   textColor?: string;
@@ -17,57 +19,77 @@ export interface N1Props {
 }
 
 const defaultProps = {
+  specialItems: [],
   backgroundColor: 'bg-blue-500',
   textColor: 'text-white',
   textCase: 'uppercase',
 };
 
 export function N1(props: N1Props) {
-  const { items, heading, backgroundColor, textColor, textCase } = props;
+  const { items, specialItems, heading, backgroundColor, textColor, textCase } = props;
 
   return (
-    <nav
-      className={classNames(
-        backgroundColor,
-        'relative mb-3 flex flex-wrap items-center justify-between px-2 py-3'
-      )}
-    >
-      <div className="container mx-auto flex flex-wrap items-center justify-between px-4">
+    <nav className={classNames(backgroundColor, 'relative mb-3 px-2 py-3')}>
+      {/* Container */}
+      <div className="w-full mx-5 flex grow flex-row">
+
         {/* Heading */}
-        <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
+        <div className="relative mr-4 ml-10 py-2">
           <p
             className={classNames(
               textColor,
               textCase,
-              'mr-4 inline-block whitespace-nowrap py-2 text-sm font-bold leading-relaxed'
+              'inline-block whitespace-nowrap text-lg font-bold leading-relaxed'
             )}
           >
             {heading}
           </p>
         </div>
+
         {/* Items */}
-        <div className="flex grow items-center lg:flex">
-          <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
+        <div className="ml-10 flex items-center">
+          <ul className="ml-auto flex list-none flex-row">
             {items.map((item: N1Item) => (
-              <li key={item.name}>
+              <li key={item.label}>
                 <a
                   className={classNames(
                     textColor,
                     textCase,
-                    'flex cursor-pointer items-center px-3 py-2 text-xs font-bold leading-snug hover:opacity-75'
+                    'ml-5 cursor-pointer items-center px-3 py-2 text-xs font-bold leading-snug hover:opacity-75'
                   )}
-                  href={item.href ? item.href : ''}
+                  href={item.href}
                 >
-                  <i className={classNames(textColor, 'text-lg opacity-75')} />
-                  <span className="ml-2">{item.name}</span>
+                  {item.label}
                 </a>
               </li>
             ))}
           </ul>
         </div>
+
+        {/* Special Items */}
+        {(specialItems !== undefined && specialItems.length > 0) && (
+          <div className="flex mx-16 grow items-center">
+            <ul className="ml-auto flex list-none flex-row">
+              {specialItems.map((item: N1Item) => (
+                <li key={item.label}>
+                  <a
+                    className={classNames(
+                      textColor,
+                      textCase,
+                      'ml-5 cursor-pointer items-center px-3 py-2 text-xs font-bold leading-snug hover:opacity-75'
+                    )}
+                    href={item.href}
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
 }
 
-N1.defaultProps = { ...defaultProps };
+N1.defaultProps = defaultProps;
