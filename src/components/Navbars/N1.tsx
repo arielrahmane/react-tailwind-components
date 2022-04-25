b3e1d28b0c30f77c084d2a26ed649547b94162a1
arielrahmane <arielrahmane@gmail.com>
2022-04-25 18:45:37 +1000

    responsive styling

--- src/components/Navbars/N1.tsx
+++ src/components/Navbars/N1.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { MenuIcon } from '@heroicons/react/outline';
 import React from 'react';
 
 export interface N1Item {
@@ -29,27 +30,54 @@ export function N1(props: N1Props) {
   const { items, specialItems, heading, backgroundColor, textColor, textCase } = props;
 
   return (
-    <nav className={classNames(backgroundColor, 'relative mb-3 px-2 py-3')}>
-      {/* Container */}
-      <div className="w-full mx-5 flex grow flex-row">
+    <div
+      className={classNames(
+        backgroundColor,
+        'relative flex w-full grow flex-row px-2 py-3'
+      )}
+    >
+      <div className="relative text-white lg:hidden">
+        <MenuIcon className="py-2 w-12 h-12" />
+      </div>
 
-        {/* Heading */}
-        <div className="relative mr-4 ml-10 py-2">
-          <p
-            className={classNames(
-              textColor,
-              textCase,
-              'inline-block whitespace-nowrap text-lg font-bold leading-relaxed'
-            )}
-          >
-            {heading}
-          </p>
-        </div>
+      {/* Heading */}
+      <div className="relative mr-4 ml-10 py-2">
+        <p
+          className={classNames(
+            textColor,
+            textCase,
+            'inline-block whitespace-nowrap text-lg font-bold leading-relaxed'
+          )}
+        >
+          {heading}
+        </p>
+      </div>
 
-        {/* Items */}
-        <div className="ml-10 flex items-center">
+      {/* Items */}
+      <div className="hidden lg:ml-10 lg:flex lg:items-center py-2">
+        <ul className="ml-auto flex list-none flex-row">
+          {items.map((item: N1Item) => (
+            <li key={item.label}>
+              <a
+                className={classNames(
+                  textColor,
+                  textCase,
+                  'ml-5 cursor-pointer items-center px-3 py-2 text-xs font-bold leading-snug hover:opacity-75'
+                )}
+                href={item.href}
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Special Items */}
+      {specialItems !== undefined && specialItems.length > 0 && (
+        <div className="mr-10 hidden grow items-center py-2 sm:flex">
           <ul className="ml-auto flex list-none flex-row">
-            {items.map((item: N1Item) => (
+            {specialItems.map((item: N1Item) => (
               <li key={item.label}>
                 <a
                   className={classNames(
@@ -65,30 +93,8 @@ export function N1(props: N1Props) {
             ))}
           </ul>
         </div>
-
-        {/* Special Items */}
-        {(specialItems !== undefined && specialItems.length > 0) && (
-          <div className="flex mx-16 grow items-center">
-            <ul className="ml-auto flex list-none flex-row">
-              {specialItems.map((item: N1Item) => (
-                <li key={item.label}>
-                  <a
-                    className={classNames(
-                      textColor,
-                      textCase,
-                      'ml-5 cursor-pointer items-center px-3 py-2 text-xs font-bold leading-snug hover:opacity-75'
-                    )}
-                    href={item.href}
-                  >
-                    {item.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </nav>
+      )}
+    </div>
   );
 }
 
